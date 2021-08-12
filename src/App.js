addd91679aa87b6f7613f7e9a59a3498942a2a70
James Beswick <james@Jamess-MacBook-Pro.local>
2021-08-12 18:03:01 +0100

    working with nested routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 
 const App = () => {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productID">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
 };
 
 export default App;
+
+//domain.com/product-detail/:<any> is a dynamic link
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,22 @@
+const { Link } = require('react-router-dom');
+
 const Products = () => {
-  return <h1>Products</h1>;
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Stuff</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
